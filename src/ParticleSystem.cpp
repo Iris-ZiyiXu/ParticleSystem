b32d2161e8e731b53dc12dc677afbdda02f3f677
Iris-ZiyiXu <iris.ziyixu@gmail.com>
2024-11-26 00:24:41 -0800

    Initialized file

---
+++ src/ParticleSystem.cpp
@@ -0,0 +1,12 @@
+#include "ParticleSystem.hpp"
+
+#include "Random.h"
+#include "Transform.hpp"
+
+#include <iostream>
+
+#include <glad/glad.h>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/gtx/compatibility.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
