58287b3c15ed62cd57388c8468408d09d45d7546
Iris-ZiyiXu <iris.ziyixu@gmail.com>
2024-11-26 00:24:21 -0800

    Implemented header file

---
+++ include/ParticleSystem.hpp
@@ -0,0 +1,34 @@
+#ifndef PARTICLESYSTEM_HPP
+#define PARTICLESYSTEM_HPP
+
+#if defined(LINUX) || defined(MINGW)
+    #include <SDL2/SDL.h>
+#else // This works for Mac
+    #include <SDL.h>
+#endif
+
+struct ParticleProps
+{
+  glm::vec2 Position;
+  glm::vec2 Velocity, VelocityVariation;
+  glm::vec4 ColorBegin, ColorEnd;
+  float SizeBegin, SizeEnd, SizeVariation;
+  float LifeTime = 1.0f;
+};
+
+class ParticleSystem()
+{
+public:
+  ParticleSystem();
+
+  void Update(float timestep);
+  void Init();
+  void Render(unsigned int screenWidth, unsigned int screenHeight);
+  void Emit(const ParticleProps &particleProps);
+
+private:
+  std::vector<Particle> m_ParticlePool;
+  uint32_t m_PoolIndex = 999;
+};
+
+#endif
