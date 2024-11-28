a23841f9b4c94473ae9fa6e69c1834b5f63f5ffd
Iris-ZiyiXu <iris.ziyixu@gmail.com>
2024-11-27 20:24:08 -0800

    Updated vertex shader

--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -1,37 +1,17 @@
 #version 330 core
 
 layout(location=0)in vec3 position; 
-layout(location=1)in vec3 normals;
-layout(location=2)in vec2 texCoord;
-layout(location=3)in vec3 tangents; 
-layout(location=4)in vec3 bitangents; 
 
-out vec2 v_texCoord;
-out vec3 TangentLightPos;
-out vec3 TangentViewPos;
-out vec3 TangentFragPos;
+out vec4 theColor;
 
 uniform mat4 modelTransformMatrix;
+uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
+uniform vec4 color;
 
 void main()
 {
-    // Calculate position in world space
-    vec3 FragPos = vec3(modelTransformMatrix * vec4(position, 1.0f));
+    gl_Position = projectionMatrix * viewMatrix * modelTransformMatrix * vec4(position, 1.0f);
 
-    // Create the TBN matrix (Tangent, Bitangent, Normal)
-    mat3 TBN = transpose(mat3(tangents, bitangents, normals));
-
-    // Transform positions into tangent space
-    TangentLightPos = TBN * (lightPos - FragPos);
-    TangentViewPos  = TBN * (viewPos - FragPos);
-    TangentFragPos  = TBN * FragPos;
-
-    // Pass texture coordinates to the fragment shader
-    v_texCoord = texCoord;
-
-    // Final position
-    gl_Position = projectionMatrix * modelTransformMatrix * vec4(position, 1.0f);
+    theColor = color;
 }
