730079d740b6c6d6dacf5be9be000666b8aa61d6
Iris-ZiyiXu <iris.ziyixu@gmail.com>
2024-11-27 20:16:17 -0800

    Implemented Init function

--- src/ParticleSystem.cpp
+++ src/ParticleSystem.cpp
@@ -39,3 +39,32 @@ void ParticleSystem::Update(float timestep)
     particle.Velocity.y += (0.01f * timestep);
   }
 }
+
+void ParticleSystem::Init()
+{
+  // vertices for quad
+  m_vertices = 
+  {
+    -0.1f, -0.1f, 0.0f,
+    0.1f,  -0.1f, 0.0f,
+     0.1f,  0.1f, 0.0f,
+     -0.1f, 0.1f, 0.0f
+  };
+  // indices for quad
+  m_indices = 
+  {
+    0, 1, 2,
+    2, 3, 0
+  };
+
+  // vertexBuffer setup
+  m_vertexBufferLayout.CreatePositionBufferLayout(
+    m_vertices.size(), m_indices.size(), m_vertices.data(), m_indices.data()
+  );
+
+  // shader setup
+  std::string vertexShader = m_shader.LoadShader("./shaders/vert.glsl");
+  std::string fragmentShader = m_shader.LoadShader("./shaders/frag.glsl");
+
+  m_shader.CreateShader(vertexShader, fragmentShader);
+}
