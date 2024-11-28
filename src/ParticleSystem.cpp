bd18560047bc0ce60fc4985271e1041562402c3c
Iris-ZiyiXu <iris.ziyixu@gmail.com>
2024-11-27 19:55:30 -0800

    Implemented Update function

--- src/ParticleSystem.cpp
+++ src/ParticleSystem.cpp
@@ -15,3 +15,27 @@ ParticleSystem::ParticleSystem()
 {
   m_ParticlePool.resize(1000);
 }
+
+void ParticleSystem::Update(float timestep)
+{
+  m_vertexBufferLayout.Bind();
+  m_shader.Bind();
+  for (auto& particle : m_PoolIndex)
+  {
+    // If particle is unused, skip
+    if (!particle.active)
+    {
+      continue;
+    }
+    // If particle is dead, skip
+    if (particle.lifeRemaining <= 0.0f)
+    {
+      particle.active = false;
+      continue;
+    }
+    // Update each active particle
+    particle.lifeRemaining -= timestep;
+    particle.Position += particle.Velocity * timestep;
+    particle.Velocity.y += (0.01f * timestep);
+  }
+}
